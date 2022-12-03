c960eb4c1436fa63acc58d404f45e0064ce7212c
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-12-03 13:09:21 +0700

    email

--- resources/views/admin/email.blade.php
+++ resources/views/admin/email.blade.php
@@ -1,18 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-    <style>
-        body{
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $subject }}</title>
+    <style type="text/css">
+        body {
+            width: 100% !important;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+            margin: 0;
+            padding: 0;
             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         }
-        p,pre{
-            font-size: 16px;
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+        #backgroundTable {
+            margin: 0;
+            padding: 0;
+            width: 100% !important;
+            line-height: 100% !important;
+        }
+
+        table td {
+            border-collapse: collapse;
+        }
+
+        table {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
         }
     </style>
 </head>
+
 <body>
-    <p>Halo, {{$nama}}!</p>
-    <pre>{{$body}}</pre>
+    <table cellpadding="0" cellspacing="0" border="0" id="backgroundTable">
+        <tr>
+            <td valign="top">Halo, {{ $nama }}!</td>
+        </tr>
+        <tr>
+            <td valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+            <td valign="top">{{ $body }}</td>
+        </tr>
+    </table>
 </body>
+
 </html>
