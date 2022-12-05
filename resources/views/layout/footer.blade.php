d777b9eb538aba378de1542c667df15fecaf1a0a
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-12-05 10:39:04 +0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-center text-lg-start" style="postition:fixed;bottom:0;width:100%">
     <div class="text-center p-3 text-light">
-      &copy; 2022 Copyright Azhure Raven & Kazurichi, All Rights Reserved.
+      &copy; 2022 Copyright Abraham Arthur Fendy (220116899) & Kevin Jonathan (220116926), All Rights Reserved.
     </div>
 </footer>
